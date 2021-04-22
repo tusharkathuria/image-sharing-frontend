213181e6f6a1962f7fddefaf3e3b3ea043f300d6
Tushar Kathuria <tushar.kathuria@compass.com>
2021-04-22 15:24:10 +0530

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = 'https://z042ifoiqi.execute-api.ap-south-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'dev-rr6wtngq.us.auth0.com',
+  clientId: 'IVp6699QHU0q2caZTLFr4PTtCqejAsG7',
+  callbackUrl: 'http://localhost:3000/callback'
+}
